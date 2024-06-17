8d2dc9dcbd0097f84b5052e1c22527887a501bd0
iamTheMike <159140657+iamTheMike@users.noreply.github.com>
2024-06-17 19:53:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);//start
 
+function plusSlides(n) {
+  showSlides(slideIndex += n); //ใช้กับ arrow
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n); //ใช้กับ Dot
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length)
+  {slideIndex = 1;} 
+  if (n < 1)
+  {slideIndex = slides.length;}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  //ลบ รูป display ออก
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", ""); //ลบ class   active ออก 
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active "; //ต้องวรรคไม่งั้น class จะติดกันเป็น class ใหม่
+}
